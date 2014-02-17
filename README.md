93577a8412909c96a861f2c4f85c6377357c8706
Eetu Lampsijärvi <tulitomaatti@gmail.com>
2014-02-17 15:09:29 +0200

    Added some initial includes and a makefile.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 edice
 =====
 
-Electronic dice for an electronics course.
+Electronic dice for an electronics course / AVR Stuff maybe. 
+
+
---
+++ edice.c
@@ -0,0 +1,19 @@
+/* edice test/main file */
+
+#define F_CPU 16000000UL /* 16MHz for atmega328 */
+#define BAUD 9600
+
+#include <avr/interrupt.h>
+#include <avr/power.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
+#include <util/setbaud.h>
+#include <stdlib.h>
+#include <avr/io.h>
+#include <stdint.h>
+
+int main() {
+	return 0;
+}
+
+
