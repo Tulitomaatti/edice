cfc51c26b8612cdb27825832add5985f75e8d009
Eetu Lampsijärvi <tulitomaatti@gmail.com>
2014-04-26 17:35:01 +0300

    reorganised

--- flash-arduino.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin/avrdude -C/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/etc/avrdude.conf -v -v -v -v -patmega328p -carduino -P/dev/tty.usbmodemfd121 -b115200 -D -Uflash:w:$1:i 
